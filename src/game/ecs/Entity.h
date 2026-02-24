45c82d5a075dc120ae7eb64e539a30e0fec96711
YutoImata <yuto.imata.wine@gmail.com>
2026-02-25 04:38:25 +0900

    feat: Entityクラスを追加

---
+++ src/game/ecs/Entity.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace game::ecs
+{
+    using EntityId = uint32_t;
+
+    // 0 は無効IDとして扱う
+    constexpr EntityId INVALID_ENTITY_ID = 0;
+
+    // ECSにおけるEntityは単なるIDラッパー
+    class Entity
+    {
+    public:
+        explicit Entity(EntityId id) : m_id(id) {}
+
+        EntityId getId() const { return m_id; }
+        bool isValid() const { return m_id != INVALID_ENTITY_ID; }
+
+        bool operator==(const Entity& other) const { return m_id == other.m_id; }
+        bool operator!=(const Entity& other) const { return m_id != other.m_id; }
+
+    private:
+        EntityId m_id;
+    };
+}
