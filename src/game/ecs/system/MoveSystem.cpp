ad5a8c4d7d753b5c5d36a9de06d3518dfb046984
YutoImata <yuto.imata.wine@gmail.com>
2026-02-26 03:15:58 +0900

    feat: MoveSystemを作成中

---
+++ src/game/ecs/system/MoveSystem.cpp
@@ -0,0 +1,22 @@
+﻿#include "MoveSystem.h"
+#include "game/ecs/component/InputComponent.h"
+#include "game/ecs/component/VelocityComponent.h"
+
+namespace game::ecs::system
+{
+	MoveSystem::MoveSystem(ComponentManager& componentManager, EntityId playerId, float moveSpeed)
+		: m_componentManager(componentManager)
+		, m_playerId(playerId)
+		, m_moveSpeed(moveSpeed)
+	{
+	}
+
+	void MoveSystem::update(float deltaTime)
+	{
+		auto& input = m_componentManager.get<component::InputComponent>(m_playerId);
+		auto& velocity = m_componentManager.get<component::VelocityComponent>(m_playerId);
+
+		velocity.m_velocity.x = input.m_moveX * m_moveSpeed;
+		velocity.m_velocity.z = input.m_moveZ * m_moveSpeed;
+	}
+}
---
+++ src/game/ecs/system/MoveSystem.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "game/ecs/ISystem.h"
+#include "game/ecs/ComponentManager.h"
+#include "game/ecs/Entity.h"
+
+namespace game::ecs::system
+{
+    /**
+     * @brief 入力を元に速度を計算するSystem
+     */
+    class MoveSystem : public ISystem
+    {
+        MoveSystem(ComponentManager& componentManager, EntityId playerId, float moveSpeed);
+        void update(float deltaTime) override;
+
+    private:
+        ComponentManager& m_componentManager;
+        EntityId m_playerId;
+        float m_moveSpeed;
+    };
+}
