9f129728d74b3d059cb9bdaaf1433bfc2a1a9e99
YutoImata <yuto.imata.wine@gmail.com>
2026-02-26 03:06:20 +0900

    feat: キーボード入力をInputComponentに反映するSystemを作成

---
+++ src/game/ecs/system/InputSystem.cpp
@@ -0,0 +1,28 @@
+﻿#include "InputSystem.h"
+#include <DxLib.h>
+#include "game/ecs/component/InputComponent.h"
+
+namespace game::ecs::system
+{
+	InputSystem::InputSystem(ComponentManager& componentManager, EntityId playerId)
+		: m_componentManager(componentManager)
+		, m_playerId(playerId)
+	{
+	}
+
+	void InputSystem::update(float deltaTime)
+	{
+		auto& input = m_componentManager.get<component::InputComponent>(m_playerId);
+
+		// キーを離したときに前フレームの値が残らないように初期化
+		input.m_moveX = 0.0f;
+		input.m_moveZ = 0.0f;
+		input.m_jumpPressed = false;
+
+		if (CheckHitKey(KEY_INPUT_D)) input.m_moveX = 1.0f;
+		if (CheckHitKey(KEY_INPUT_A)) input.m_moveX = -1.0f;
+		if (CheckHitKey(KEY_INPUT_W)) input.m_moveZ = 1.0f;
+		if (CheckHitKey(KEY_INPUT_S)) input.m_moveZ = -1.0f;
+		if (CheckHitKey(KEY_INPUT_SPACE)) input.m_jumpPressed = true;
+	}
+}
---
+++ src/game/ecs/system/InputSystem.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "game/ecs/ISystem.h"
+#include "game/ecs/ComponentManager.h"
+#include "game/ecs/Entity.h"
+
+namespace game::ecs::system
+{
+	/**
+	 * @brief キーボード入力をInputComponentに反映するSystem
+	 */
+	class InputSystem : public ISystem
+	{
+	public:
+		InputSystem(ComponentManager& componentManager, EntityId playerId);
+		void update(float deltaTime) override;
+
+	private:
+		ComponentManager& m_componentManager;
+		EntityId m_playerId;
+	};
+}
