5b75e76cbe7136992f614a61035b54397edfddb5
YutoImata <yuto.imata.wine@gmail.com>
2026-02-27 00:04:11 +0900

    feat: カメラのコンポーネントの作成

---
+++ src/game/ecs/component/CameraComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <DxLib.h>
+
+namespace game::ecs::component
+{	/**
+	 * @brief カメラの情報を持つコンポーネント
+	 */
+	struct CameraComponent
+	{
+		static constexpr float OFFSET_Y = 800.0f;
+		static constexpr float OFFSET_Z = -600.0f;
+
+		VECTOR m_offset = { 0.0f, OFFSET_Y, OFFSET_Z }; // Playerからカメラまでの相対位置
+		VECTOR m_target = { 0.0f,0.0f, 0.0f };
+		VECTOR m_position = { 0.0f, 0.0f, 0.0f };
+	};
+}
