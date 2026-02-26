cfec1058763cfbe60abccf417e94f5106aef4bb8
YutoImata <yuto.imata.wine@gmail.com>
2026-02-27 01:38:20 +0900

    refactor: アーキテクチャに則ってカメラと描画クラスをengine層に移動

---
+++ src/engine/Renderer.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "core/Vector3.h"
+
+namespace engine
+{
+    /**
+     * @brief 3D描画を担当するクラス
+     */
+    class Renderer
+    {
+    public:
+        void drawModel(int modelHandle, const core::Vector3& position);
+    };
+}
