a85c32c19f377d5fff9b65881333c27ccb1740db
YutoImata <yuto.imata.wine@gmail.com>
2026-02-27 01:05:19 +0900

    feat:モデルを読み込むクラスを作成

---
+++ src/engine/ResourceManager.cpp
@@ -0,0 +1,42 @@
+﻿#include "ResourceManager.h"
+#include <DxLib.h>
+#include "utility/LogUtil.h"
+
+namespace engine
+{
+	int ResourceManager::loadModel(const std::string& filePath)
+	{
+		// キャッシュにある場合は重複しないように
+		auto it = m_modelCache.find(filePath);
+		if (it != m_modelCache.end()) return it->second;
+
+		int handle = MV1LoadModel(filePath.c_str());
+		if (handle == -1)
+		{
+			utility::LogUtil::error("モデルの読み込みに失敗しました: %s", filePath.c_str());
+			return -1;
+		}
+
+		m_modelCache[filePath] = handle;
+		return handle;
+	}
+
+	void ResourceManager::unloadModel(const std::string& filePath)
+	{
+		auto it = m_modelCache.find(filePath);
+		if (it == m_modelCache.end()) return;
+
+		MV1DeleteModel(it->second);
+		m_modelCache.erase(it);
+	}
+
+	void ResourceManager::unloadAll()
+	{
+		for (auto& [path, handle] : m_modelCache)
+		{
+			MV1DeleteModel(handle);
+		}
+
+		m_modelCache.clear();
+	}
+}
---
+++ src/engine/ResourceManager.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <unordered_map>
+#include <string>
+
+namespace engine
+{
+	/**
+	 * @brief リソースの読み込み・管理を担当するクラス
+	 */
+	class ResourceManager
+	{
+	public:
+		int loadModel(const std::string& filePath);
+		void unloadModel(const std::string& filePath);
+		void unloadAll();
+
+	private:
+		// ファイルパスをキーにしてモデルを管理
+		std::unordered_map<std::string, int> m_modelCache;
+
+	};
+}
