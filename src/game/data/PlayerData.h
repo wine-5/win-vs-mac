7a283214614ca19c53775c025f4a81dc9217de27
YutoImata <yuto.imata.wine@gmail.com>
2026-03-04 05:03:57 +0900

    feat: Playerのデータを一括で管理するクラスを作成

---
+++ src/game/data/PlayerData.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+
+namespace game::data
+{
+	/**
+	 * @brief Playerのデータを保持するクラス
+	 */
+	class PlayerData
+	{
+	public:
+		const std::string& getModelPath()    const { return m_modelPath; }
+		const std::string& getIdleAnimPath() const { return m_idleAnimPath; }
+		const std::string& getWalkAnimPath() const { return m_walkAnimPath; }
+		float              getMoveSpeed()    const { return m_moveSpeed; }
+
+	private:
+		std::string m_modelPath = "assets/model/Player.mv1";
+		std::string m_idleAnimPath = "assets/animation/Player_Idle.mv1";
+		std::string m_walkAnimPath = "assets/animation/Player_Walking.mv1";
+		float       m_moveSpeed = 5.0f;
+	};
+}
