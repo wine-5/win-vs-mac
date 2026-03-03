9fc62d5eb1d38c58b737eba8516e21c88c416a19
YutoImata <yuto.imata.wine@gmail.com>
2026-03-03 23:17:33 +0900

    feat: アニメーションのシステムを作成

---
+++ src/game/system/AnimationSystem.cpp
@@ -0,0 +1,3 @@
+﻿#include "AnimationSystem.h"
+#include "game/component/RenderComponent.h"
+//#include ""	
---
+++ src/game/system/AnimationSystem.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "core/ecs/ISystem.h"
+#include "core/ecs/ComponentManager.h"
+#include "core/ecs/Entity.h"
+#include "game/component/AnimationComponent.h"
+#include "game/component/VelocityComponent.h"
+#include "game/constant/PlayerAnimationState.h"
+
+namespace game::system
+{
+	/**
+	 * @brief アニメーション状態を管理・更新するSystem
+	 */
+	class AnimationSystem : public core::ecs::ISystem
+	{
+	public:
+		AnimationSystem(core::ecs::ComponentManager& componentManager,
+			core::ecs::EntityId playerId,
+			int idleAnimHandle,
+			int walkAnimHandle);
+
+		void update(float deltaTime) override;
+
+	private:
+		void changeAnimation(
+			component::AnimationComponent<constant::PlayerAnimationState>& anim,
+			int modelHandle,
+			constant::PlayerAnimationState newState,
+			int newAnimHandle);
+		
+		core::ecs::ComponentManager& m_componentManager;
+		core::ecs::EntityId m_playerId;
+		int m_idleAnimHandle;
+		int m_walkAnimHandle;
+	};
+}
