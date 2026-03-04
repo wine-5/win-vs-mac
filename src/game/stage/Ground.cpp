8410ea0e2d939e9a1fa307d0552285870e1b0732
YutoImata <yuto.imata.wine@gmail.com>
2026-03-04 20:24:58 +0900

    feat: Groundのオブジェクトクラスの作成

---
+++ src/game/stage/Ground.cpp
@@ -0,0 +1,31 @@
+﻿#include "Ground.h"
+#include "game/component/TransformComponent.h"
+#include "game/component/RenderComponent.h"
+#include "game/component/ColliderComponent.h"
+
+namespace game::stage
+{
+	Ground::Ground(core::ecs::EntityManager& entityManager,
+		core::ecs::ComponentManager& componentManager,
+		int modelHandle,
+		core::Vector3 position,
+		core::Vector3 size)
+		: m_entity(entityManager.create())
+	{
+		component::TransformComponent transform;
+		transform.m_position = position;
+
+		componentManager.add<component::TransformComponent>(m_entity.getId(), transform);
+		componentManager.add<component::RenderComponent>(m_entity.getId(), { modelHandle });
+
+		component::ColliderComponent collider;
+		collider.m_size = size;
+
+		componentManager.add<component::ColliderComponent>(m_entity.getId(), collider);
+	}
+
+	core::ecs::EntityId Ground::getId() const
+	{
+		return m_entity.getId();
+	}
+}
---
+++ src/game/stage/Ground.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "core/ecs/EntityManager.h"
+#include "core/ecs/ComponentManager.h"
+#include "core/ecs/Entity.h"
+#include "core/Vector3.h"
+
+namespace game::stage
+{
+	/**
+	 * @brief 地面のオブジェクト
+	 */
+	class Ground
+	{
+	public:
+		Ground(core::ecs::EntityManager& entityManager,
+			core::ecs::ComponentManager& compnentManager,
+			int modelHandle,
+			core::Vector3 position,
+			core::Vector3 size);
+
+		core::ecs::EntityId getId() const;
+
+	private:
+		core::ecs::Entity m_entity;
+	};
+}
