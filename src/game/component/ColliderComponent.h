e39af6f410b559b932dd6263f4b225283e584398
YutoImata <yuto.imata.wine@gmail.com>
2026-03-04 20:24:42 +0900

    feat: ColliderComponentの作成

---
+++ src/game/component/ColliderComponent.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "core/Vector3.h"
+
+namespace game::component
+{
+	/**
+	 * @brief 当たり判定の形状のデータを保持するコンポーネント
+	 */
+	struct ColliderComponent
+	{
+		core::Vector3 m_size; // 当たり判定のサイズ（幅、高さ、奥行き)
+		core::Vector3 m_offset; // 中心からのオフセット
+	};
+}
