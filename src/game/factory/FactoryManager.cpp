9e19ae1bd294d8d8b5a36c7d30104192467875e1
YutoImata <yuto.imata.wine@gmail.com>
2026-03-13 00:51:07 +0900

    feat: Factoryを統括するクラスを作成

---
+++ src/game/factory/FactoryManager.cpp
@@ -0,0 +1,23 @@
+﻿#include "FactoryManager.h"
+
+namespace game::factory
+{
+	FactoryManager::FactoryManager(
+		core::ecs::EntityManager& entityManager,
+		core::ecs::ComponentManager& componentManager,
+		core::iface::IResourceManager& resourceManager)
+		: m_playerFactory(std::make_unique<PlayerFactory>(entityManager, componentManager, resourceManager))
+		, m_groundFactory(std::make_unique<GroundFactory>(entityManager, componentManager, resourceManager))
+	{
+	}
+
+	PlayerFactory& FactoryManager::getPlayerFactory()
+	{
+		return *m_playerFactory;
+	}
+
+	GroundFactory& FactoryManager::getGroundFactory()
+	{
+		return *m_groundFactory;
+	}
+}
---
+++ src/game/factory/FactoryManager.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include "PlayerFactory.h"
+#include "GroundFactory.h"
+
+namespace game::factory
+{
+    /**
+     * @brief 全てのFactoryを統括管理するFacadeクラス
+     */
+    class FactoryManager
+    {
+    public:
+        /**
+         * @brief FactoryManagerのコンストラクタ
+         * @param entityManager EntityManagerの参照
+         * @param componentManager ComponentManagerの参照
+         * @param resourceManager IResourceManagerの参照
+         */
+        FactoryManager(
+            core::ecs::EntityManager& entityManager,
+            core::ecs::ComponentManager& componentManager,
+            core::iface::IResourceManager& resourceManager);
+
+        /**
+         * @brief PlayerFactoryを取得する
+         * @return PlayerFactoryの参照
+         */
+        PlayerFactory& getPlayerFactory();
+
+        /**
+         * @brief GroundFactoryを取得する
+         * @return GroundFactoryの参照
+         */
+        GroundFactory& getGroundFactory();
+
+    private:
+        std::unique_ptr<PlayerFactory> m_playerFactory;
+        std::unique_ptr<GroundFactory> m_groundFactory;
+    };
+}
