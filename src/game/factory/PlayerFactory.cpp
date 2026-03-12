aa0b989648324a0f520c052478878bd74d0c1d3d
YutoImata <yuto.imata.wine@gmail.com>
2026-03-13 00:03:31 +0900

    feat: PlayerFactoryの作成

---
+++ src/game/factory/PlayerFactory.cpp
@@ -0,0 +1,29 @@
+﻿#include "PlayerFactory.h"
+
+namespace game::factory
+{
+	PlayerFactory::PlayerFactory(
+		core::ecs::EntityManager& entityManager,
+		core::ecs::ComponentManager& componentManager,
+		core::iface::IResourceManager& resourceManager)
+		: m_entityManager(entityManager)
+		, m_componentManager(componentManager)
+		, m_resourceManager(resourceManager)
+	{
+	}
+
+	void PlayerFactory::create(int modelHandle, const data::PlayerData& playerData)
+	{
+		m_player = std::make_unique<actor::Player>(
+			m_entityManager,
+			m_componentManager,
+			modelHandle,
+			playerData.getColliderSize(),
+			playerData.getColliderOffset());
+	}
+
+	actor::Player& PlayerFactory::getPlayer() const
+	{
+		return *m_player;
+	}
+}
