86d0a0c143605861de95e7b4fd28144c965ffdcb
YutoImata <yuto.imata.wine@gmail.com>
2026-03-12 23:47:42 +0900

    feat: Factoryクラスに継承するべき純粋仮想クラスを作成

---
+++ src/game/factory/IFactory.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace game::factory
+{
+    /**
+     * @brief Factory基底クラス
+     * @details オブジェクトの生成と寿命管理を担当する
+     *
+     * ## 実装規約
+     * - 派生クラスは必ず `create()` メソッドを実装すること
+     * - 本来であればcreate関数を定義したいが、引数が異なるためコメントのみを記載
+     */
+    class IFactory
+    {
+    public:
+        virtual ~IFactory() = default;
+    };
+}
