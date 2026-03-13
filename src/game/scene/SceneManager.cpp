d620b7f206a3e5f7365ec4193bbe1019255f9af8
YutoImata <yuto.imata.wine@gmail.com>
2026-03-13 16:42:36 +0900

    feat: シーンを管理するクラスを作成

---
+++ src/game/scene/SceneManager.cpp
@@ -0,0 +1,16 @@
+﻿#include "SceneManager.h"
+
+namespace game::scene
+{
+	void SceneManager::update(float deltaTime)
+	{
+		if (m_currentScene)
+			m_currentScene->update(deltaTime);
+	}
+
+	void SceneManager::changeScene(SceneType sceneType, std::unique_ptr<IScene> scene)
+	{
+		m_currentSceneType = sceneType;
+		m_currentScene = std::move(scene);
+	}
+}
---
+++ src/game/scene/SceneManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include "IScene.h"
+#include "SceneType.h"
+#include <memory>
+
+namespace game::scene
+{
+	/**
+	 * @brief シーン全体を管理するクラス
+	 */
+	class SceneManager
+	{
+	public:
+		/**
+		 * @brief 現在のシーンを更新する
+		 * @param deltaTime フレーム間の時間差
+		 */
+
+		void update(float deltaTime);
+		/**
+		 * @brief シーンを変更する
+		 * @param sceneType 変更先のシーンの種類
+		 */
+
+		void changeScene(SceneType sceneType, std::unique_ptr<IScene> scene);
+
+	private:
+		std::unique_ptr<IScene> m_currentScene{};
+		SceneType m_currentSceneType;
+	};
+}
