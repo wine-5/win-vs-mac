ff8ce36be119961b6faffa8aaa6818e82f80072a
YutoImata <yuto.imata.wine@gmail.com>
2026-03-19 02:32:33 +0900

    feat: AIComponentの作成

---
+++ src/game/component/AIComponent.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "core/ecs/Entity.h"
+
+namespace game::component
+{
+	/**
+     * @brief AI追従行動を制御するコンポーネント
+     */
+	struct AIComponent
+	{
+		core::ecs::Entity m_targetEntity{ 0 };
+		float m_moveSpeed{ 2.0f };
+		float m_detectionRange{ 10.0f };
+		bool m_isActive{ true };
+	};
+}
