53a4d29dd9417eb3cc6b3f2ca8714b4fcd848826
YutoImata <yuto.imata.wine@gmail.com>
2026-03-26 15:38:58 +0900

    wip: AISystemの実装においてComponentを取得するところまで書いた

--- src/game/system/AISystem.cpp
+++ src/game/system/AISystem.cpp
@@ -4,3 +4,34 @@
 #include "game/component/VelocityComponent.h"
 #include "core/Vector3.h"
 #include <cmath>
+
+namespace game::system
+{
+	AISystem::AISystem(core::ecs::ComponentManager& componentManaager)
+		: m_componentManager(componentManaager)
+	{
+	}
+
+	void AISystem::update(float deltaTime)
+	{
+		// AIComponentを持つ全エンティティを取得
+		auto entities = m_componentManager.getAllEntities<component::AIComponent>();
+		for (auto entityId : entities)
+		{
+			auto& ai = m_componentManager.get<component::AIComponent>(entityId);
+
+			// AIが無効なら処理をスキップ
+			if (!ai.m_isActive)
+				continue;
+
+			// 追跡対象が設定されていない場合はスキップ
+			if(ai.m_targetEntity.getId() == 0)
+			continue;
+
+			auto& trasform = m_componentManager.get<component::AIComponent>(entityId);
+			auto& targetTransform = m_componentManager.get<component::TransformComponent>(ai.m_targetEntity.getId());
+
+
+		}
+	}
+}
