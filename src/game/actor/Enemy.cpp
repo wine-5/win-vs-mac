152a9311f83fb10678adab0c25c23e23ba84715f
YutoImata <yuto.imata.wine@gmail.com>
2026-04-04 02:11:22 +0900

    feat: 敵の実装を追加

---
+++ src/game/actor/Enemy.cpp
@@ -0,0 +1,42 @@
+﻿#include "Enemy.h"
+#include "game/component/TransformComponent.h"
+#include "game/component/VelocityComponent.h"
+#include "game/component/RenderComponent.h"
+#include "game/component/AnimationComponent.h"
+#include "game/component/ColliderComponent.h"
+#include "game/component/AIComponent.h"
+#include "game/component/HealthComponent.h"
+#include "game/constant/EnemyAnimationState.h"
+#include "game/constant/CollisionTag.h"
+
+namespace game::actor
+{
+	Enemy::Enemy(core::ecs::EntityManager& entityManager,
+		core::ecs::ComponentManager& componentManager,
+		int modelHandle,
+		const data::EnemyData& enemyData)
+		: m_entity{ entityManager.create() }
+	{
+		componentManager.add<component::TransformComponent>(m_entity.getId(), {});
+		componentManager.add<component::VelocityComponent>(m_entity.getId(), {});
+		componentManager.add<component::AnimationComponent<constant::EnemyAnimationState>>(m_entity.getId(), {});
+		componentManager.add<component::RenderComponent>(m_entity.getId(), { modelHandle });
+		componentManager.add<component::HealthComponent>(m_entity.getId(), {});
+
+		component::ColliderComponent collider;
+		collider.m_size = enemyData.getColliderSize();
+		collider.m_offset = enemyData.getColliderOffset();
+		collider.m_tag = constant::CollisionTag::Enemy;
+		componentManager.add<component::ColliderComponent>(m_entity.getId(), collider);
+
+		component::AIComponent ai;
+		ai.m_moveSpeed = enemyData.getMoveSpeed();
+		ai.m_detectionRange = enemyData.getDetectionRange();
+		componentManager.add<component::AIComponent>(m_entity.getId(), ai);
+	}
+
+	core::ecs::EntityId Enemy::getId() const noexcept
+	{
+		return m_entity.getId();
+	}
+}
---
+++ src/game/actor/Enemy.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "core/ecs/EntityManager.h"
+#include "core/ecs/ComponentManager.h"
+#include "core/ecs/Entity.h"
+#include "core/Vector3.h"
+#include "game/data/EnemyData.h"
+
+namespace game::actor
+{
+    /**
+     * @brief Enemyのセットアップを担当するクラス
+     */
+    class Enemy
+    {
+    public:
+        /**
+         * @brief Enemyのコンストラクタ
+         * @param entityManager EntityManagerの参照
+         * @param componentManager ComponentManagerの参照
+         * @param modelHandle モデルハンドル
+         * @param moveSpeed 移動速度
+         * @param detectionRange 索敵範囲
+         * @param colliderSize コライダーのサイズ
+         * @param colliderOffset コライダーのオフセット
+         */
+        Enemy(core::ecs::EntityManager& entityManager,
+            core::ecs::ComponentManager& componentManager,
+            int modelHandle,
+            const data::EnemyData& enemyData
+            );
+
+        /**
+         * @brief EnemyのEntityIDを取得する
+         * @return EntityID
+         */
+        [[nodiscard]] core::ecs::EntityId getId() const noexcept;
+
+    private:
+        core::ecs::Entity m_entity;
+    };
+}
