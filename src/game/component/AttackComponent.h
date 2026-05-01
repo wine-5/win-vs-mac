2289edb8d68597458c937e5fdcbbe3701dfa6fbc
YutoImata <yuto.imata.wine@gmail.com>
2026-05-01 11:03:16 +0900

    feat: 攻撃のComponentに攻撃をする要求を出すかどうかのフラグを追加

--- src/game/component/AttackComponent.h
+++ src/game/component/AttackComponent.h
@@ -2,14 +2,15 @@
 
 namespace game::component
 {
-    /**
-     * @brief 攻撃を持つコンポーネント
-     */
-    struct AttackComponent
-    {
-        float m_attackPower{ 10.0f };
-        float m_attackRange{ 100.0f };
-        float m_attackCooldown{ 2.0f };
-        float m_currentCooldown{ 0.0f };
-    };
+	/**
+	 * @brief 攻撃を持つコンポーネント
+	 */
+	struct AttackComponent
+	{
+		float m_attackPower{ 10.0f };
+		float m_attackRange{ 100.0f };
+		float m_attackCooldown{ 2.0f };
+		float m_currentCooldown{ 0.0f };
+		bool  m_attackRequested{ false };
+	};
 }
