eebfa1fb5510c57387e3a4f31c03fc2bb4954c10
YutoImata <yuto.imata.wine@gmail.com>
2026-05-04 22:30:31 +0900

    wip: ファイル選択クラスの実装を書いている途中

---
+++ src/platform/WindowFileSelector.cpp
@@ -0,0 +1,11 @@
+#include "WindowFileSelector.h"
+#include <Windows.h>
+
+namespace platform
+{
+	//std::string WindowsFileSelector::selectFile()
+	//{
+	//	char filePath[MAX_PATH]; // Windowsが保証するパスの最大文字数
+
+	//}
+}
