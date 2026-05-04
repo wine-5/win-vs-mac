5157550445ab914fa270d3db837cc25a296c92bb
YutoImata <yuto.imata.wine@gmail.com>
2026-05-04 15:00:32 +0900

    feat: jsonから攻撃関連のパラメータを設定できるようにMetaのKey、jsonにパラメータの追加

--- src/game/constant/MetadataKeys.h
+++ src/game/constant/MetadataKeys.h
@@ -11,5 +11,9 @@ namespace game::constant
 		constexpr std::string_view MOVE_SPEED = "moveSpeed";
 		constexpr std::string_view DETECTION_RANGE = "detectionRange";
 		constexpr std::string_view ATTACK_RANGE = "attackRange";
+		constexpr std::string_view MAX_HP = "maxHp";
+		constexpr std::string_view DEFENCE = "defence";
+		constexpr std::string_view ATTACK_POWER = "attackPower";
+		constexpr std::string_view ATTACK_COOLDOWN = "attackCooldown";
 	}
 }
