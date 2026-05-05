40c88584761d9ed6fe0328815d74db63bb52c26e
YutoImata <yuto.imata.wine@gmail.com>
2026-05-05 20:43:55 +0900

    refactor: マジックナンバーを削除

--- src/game/data/FileExtensionBonus.h
+++ src/game/data/FileExtensionBonus.h
@@ -1,15 +1,15 @@
 #pragma once
 
 namespace game::data
-{	/**
-     * @brief 拡張子によるパラメータボーナス値を保持する構造体
-     */
+{ /**
+   * @brief 拡張子によるパラメータボーナス値を保持する構造体
+   */
 	struct FileExtensionBonus
 	{
-		float atk{ 0.0f };
-		float spd{ 0.0f };
-		float def{ 0.0f };
-		float hp{ 0.0f };
-		float attackRange{ 0.0f };
+		float atk{0.0f};
+		float spd{0.0f};
+		float def{0.0f};
+		float hp{0.0f};
+		float attackRange{0.0f};
 	};
 }
--- src/game/utility/ExtensionBonusCalculator.h
+++ src/game/utility/ExtensionBonusCalculator.h
@@ -4,37 +4,49 @@
 
 namespace game::utility
 {
-    /**
-     * @brief FileExtensionType に応じたパラメータボーナスを算出するクラス
-     */
-    class ExtensionBonusCalculator
-    {
-    public:
-        /**
-         * @brief 拡張子種別に対応する FileExtensionBonus を返す
-         * @param type ファイル拡張子グループ種別
-         * @return 対応するボーナス値
-         */
-        [[nodiscard]] static constexpr data::FileExtensionBonus calculate(data::FileExtensionType type) noexcept
-        {
-            // 拡張子の種類に応じて指示付き初期化子で加算
-            // TODO: 現在は値がハードコーディングになっているため今後はjsonもしくは無名名前空間などでマジックナンバーを削除する
-            switch (type)
-            {
-            case data::FileExtensionType::Executable:
-                return { .atk = 10.0f };
-            case data::FileExtensionType::Document:
-                return { .spd = 1.5f };
-            case data::FileExtensionType::Image:
-                return { .def = 8.0f };
-            case data::FileExtensionType::Audio:
-                return { .hp = 20.0f };
-            case data::FileExtensionType::Archive:
-                return { .atk = 3.0f, .spd = 0.5f, .def = 3.0f, .hp = 5.0f, .attackRange = 0.5f };
-            case data::FileExtensionType::Unknown:
-            default:
-                return { .attackRange = 20.0f };
-            }
-        }
-    };
+	namespace
+	{
+		constexpr float EXECUTABLE_ATK{ 10.0f };
+		constexpr float DOCUMENT_SPD{ 1.5f };
+		constexpr float IMAGE_DEF{ 8.0f };
+		constexpr float AUDIO_HP{ 20.0f };
+		constexpr float ARCHIVE_ATK{ 3.0f };
+		constexpr float ARCHIVE_SPD{ 0.5f };
+		constexpr float ARCHIVE_DEF{ 3.0f };
+		constexpr float ARCHIVE_HP{ 5.0f };
+		constexpr float ARCHIVE_RANGE{ 0.5f };
+		constexpr float UNKNOWN_RANGE{ 20.0f };
+	}
+
+	/**
+	 * @brief FileExtensionType に応じたパラメータボーナスを算出するクラス
+	 */
+	class ExtensionBonusCalculator
+	{
+	public:
+		/**
+		 * @brief 拡張子種別に対応する FileExtensionBonus を返す
+		 * @param type ファイル拡張子グループ種別
+		 * @return 対応するボーナス値
+		 */
+		[[nodiscard]] static constexpr data::FileExtensionBonus calculate(data::FileExtensionType type) noexcept
+		{
+			switch (type)
+			{
+			case data::FileExtensionType::Executable:
+				return { .atk = EXECUTABLE_ATK };
+			case data::FileExtensionType::Document:
+				return { .spd = DOCUMENT_SPD };
+			case data::FileExtensionType::Image:
+				return { .def = IMAGE_DEF };
+			case data::FileExtensionType::Audio:
+				return { .hp = AUDIO_HP };
+			case data::FileExtensionType::Archive:
+				return { .atk = ARCHIVE_ATK, .spd = ARCHIVE_SPD, .def = ARCHIVE_DEF, .hp = ARCHIVE_HP, .attackRange = ARCHIVE_RANGE };
+			case data::FileExtensionType::Unknown:
+			default:
+				return { .attackRange = UNKNOWN_RANGE };
+			}
+		}
+	};
 }
