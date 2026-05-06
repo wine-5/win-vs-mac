5edbe168cf1e9b00418eaa0d98565627f190834f
YutoImata <yuto.imata.wine@gmail.com>
2026-05-06 15:26:10 +0900

    feat: 動的にも対応できるようにテキストを表示するラッパークラスを作成

---
+++ src/game/ui/Label.cpp
@@ -0,0 +1,38 @@
+#include "Label.h"
+#include <utility>
+
+namespace game::ui
+{
+    Label::Label(std::string text, int x, int y, unsigned int color, int fontSize)
+        : m_text{ std::move(text) }
+        , m_x{ x }
+        , m_y{ y }
+        , m_fontSize{ fontSize }
+        , m_color{ color }
+        , m_visible{ true }
+    {
+    }
+
+    void Label::update()
+    {
+        // 状態変化がないため空実装
+    }
+
+    void Label::draw(core::iface::IUIRenderer& uiRenderer) const
+    {
+        if (!m_visible) return;
+
+        // TODO: 将来的にここにFontSizeの引数を追加する
+        uiRenderer.drawText(m_x, m_y, m_text.c_str(), m_color);
+    }
+
+    void Label::setVisible(bool visible)
+    {
+        m_visible = visible;
+    }
+
+    void Label::setText(const std::string& text)
+    {
+        m_text = text;
+    }
+}
---
+++ src/game/ui/Label.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "IUIElement.h"
+#include "game/constant/UI.h"
+#include <string>
+
+namespace game::ui
+{
+	/**
+	 * @brief テキストを描画するUI要素
+	 */
+	class Label : public IUIElement
+	{
+	public:
+		/**
+		 * @brief Labelのコンストラクタ
+		 * @param text 表示するテキスト
+		 * @param x X座標
+		 * @param y Y座標
+		 * @param color 文字色（ARGB）
+		 * @param fontSize フォントサイズ（省略時はDEFAULT_FONT_SIZE）
+		 */
+		Label(std::string text, int x, int y, unsigned int color,
+			int fontSize = game::constant::ui::DEFAULT_FONT_SIZE);
+
+		void update() override;
+		void draw(core::iface::IUIRenderer& uiRenderer) const override;
+		void setVisible(bool visible) override;
+
+		/**
+		 * @brief テキストを変更する
+		 * @param text 新しいテキスト
+		 */
+		void setText(const std::string& text);
+
+	private:
+		std::string m_text;
+		int m_x{};
+		int m_y{};
+		int m_fontSize{};
+		unsigned int m_color{};
+		bool m_visible{};
+
+	};
+}
