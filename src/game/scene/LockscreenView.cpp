5e751f3e3ec53a863f9de7bb06b8e8fb17f932b8
YutoImata <yuto.imata.wine@gmail.com>
2026-05-08 19:22:31 +0900

    wip: ロック画面の描画の実装クラスの作成中

---
+++ src/game/scene/LockscreenView.cpp
@@ -0,0 +1,29 @@
+#include "LockscreenView.h"
+#include "core/utility/Color.h"
+#include "core/constant/UI.h"
+#include <cmath>
+#include <ctime>
+#include <string>
+
+namespace game::scene
+{
+	class LockscreenView
+	{		
+		LockscreenView(core::iface::IUIRenderer& uiRenderer,
+			core::iface::IScreen& screen,
+			std::string mainFontName)
+		{
+		}
+
+		void update(float deltaTime)
+		{
+
+		}
+
+		void draw(int offsetY) const
+		{
+
+		}
+
+	};
+}
