6c5ca55d15025d40af6a4625ba059badab9e0e49
YutoImata <yuto.imata.wine@gmail.com>
2026-05-14 02:08:28 +0900

    feat: シングルトンの基底クラスを作成

---
+++ src/core/base/Singleton.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace core::base
+{
+	/**
+	 * @brief シングルトン基底クラスのテンプレート
+	 * @tparam T シングルトンにするクラス型
+	 */
+	template<typename T>
+	class Singleton
+	{
+	public:
+		/**
+		 * @brief シングルトンのインスタンスを取得
+		 * @return 唯一のインスタンスへの参照
+		 */
+		static T& getInstance()
+		{
+			static T instance;
+			return instance;
+		}
+
+		// コピーコンストラクタ削除にして禁止にする
+		Singleton(const Singleton&) = delete;
+		Singleton& operator=(const Singleton&) = delete;
+
+		// ムーブセマンティクス削除
+		Singleton(Singleton&&) = delete;
+		Singleton& operator=(Singleton&&) = delete;
+
+	protected:
+		/**
+		 * @brief デフォルトコンストラクタ
+		 */
+		Singleton() = default;
+
+		/**
+		 * @brief デストラクタ
+		 */
+		~Singleton() = default;
+	};
+}
