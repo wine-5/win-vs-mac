be9cce098c2c9f16e5492a11f104dd83a6dabdd6
YutoImata <yuto.imata.wine@gmail.com>
2026-05-15 20:33:10 +0900

    refactor: ResourceManagerからJobのリソースの処理を移植

---
+++ src/infrastructure/repository/JobRepository.cpp
@@ -0,0 +1,38 @@
+#include "JobRepository.h"
+#include <fstream>
+#include <stdexcept>
+#include "thirdparty/nlohmann/json.hpp"
+
+namespace infrastructure
+{
+	JobRepository::JobRepository()
+	{
+		std::ifstream file("assets/data/jobData.json");
+		if (!file.is_open())
+			throw std::runtime_error("assets/data/jobData.json not found");
+
+		auto json = nlohmann::json::parse(file);
+		const auto& jobs{ json["jobs"] };
+
+		for (size_t i = 0; i < jobs.size() && i < m_jobTable.size(); ++i)
+		{
+			m_jobTable[i].m_id        = jobs[i]["id"];
+			m_jobTable[i].m_name      = jobs[i]["name"];
+			m_jobTable[i].m_skillName = jobs[i]["skillName"];
+			m_jobTable[i].m_hp        = jobs[i]["hp"];
+			m_jobTable[i].m_atk       = jobs[i]["atk"];
+			m_jobTable[i].m_def       = jobs[i]["def"];
+			m_jobTable[i].m_spd       = jobs[i]["spd"];
+		}
+	}
+
+	int JobRepository::getJobCount() const noexcept
+	{
+		return core::constant::JOB_COUNT;
+	}
+
+	core::iface::JobInfo JobRepository::getJobInfo(core::constant::JobType jobType) const
+	{
+		return m_jobTable[static_cast<int>(jobType)];
+	}
+}
--- src/infrastructure/repository/JobRepository.h
+++ src/infrastructure/repository/JobRepository.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <array>
+#include "core/interface/IJobProvider.h"
+#include "core/constant/JobType.h"
+
+namespace infrastructure
+{
+	class JobRepository
+	{
+	public:
+		JobRepository();
+
+		int getJobCount() const noexcept;
+		core::iface::JobInfo getJobInfo(core::constant::JobType jobType) const;
+
+	private:
+		std::array<core::iface::JobInfo, core::constant::JOB_COUNT> m_jobTable;
+	};
+}
