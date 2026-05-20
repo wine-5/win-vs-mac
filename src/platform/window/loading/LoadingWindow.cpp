5ca88d216d99161f4dafcd91586798f72b3227e7
YutoImata <yuto.imata.wine@gmail.com>
2026-05-20 14:11:58 +0900

    feat: ローディング画面のWindowAPIの実装クラスを作成

---
+++ src/platform/window/loading/LoadingWindow.cpp
@@ -0,0 +1,49 @@
+#include "LoadingWindow.h"
+#include <nlohmann/json.hpp>
+
+namespace platform::window::loading
+{
+	LoadingWindow::LoadingWindow(int x, int y, int width, int height) noexcept
+		: WindowBase(L"LoadingWindowClass", L"コマンド プロンプト - dungeon_load.exe", x, y, width, height)
+	{
+	}
+
+	void LoadingWindow::setOnLoadingComplete(std::function<void()> callback) noexcept
+	{
+		m_onLoadingComplete = callback;
+	}
+
+	void LoadingWindow::onCreateControls(HWND hwnd)
+	{
+		// WebView2 からのメッセージハンドラを設定（initialize の前に設定）
+		m_webView.setOnMessage([this](const std::string& json) {
+			handleMessage(json);
+		});
+
+		// WebView2 を初期化
+		m_webView.initialize(hwnd, L"https://game.web/loading/loading.html");
+	}
+
+	LRESULT LoadingWindow::onMessage(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+	{
+		return WindowBase::onMessage(hwnd, msg, wParam, lParam);
+	}
+
+	void LoadingWindow::handleMessage(const std::string& json) noexcept
+	{
+		try
+		{
+			auto data = nlohmann::json::parse(json);
+
+			if (data.contains("type") && data["type"] == "loadingComplete")
+			{
+				if (m_onLoadingComplete)
+					m_onLoadingComplete();
+			}
+		}
+		catch (const std::exception&)
+		{
+			// JSON パースエラーは無視
+		}
+	}
+}
