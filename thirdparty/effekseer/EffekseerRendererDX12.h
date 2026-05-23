109ff554772a066822d884dc9027a34f0bc03e71
YutoImata <yuto.imata.wine@gmail.com>
2026-05-23 21:31:21 +0900

    build: Effekseerのlibを追加

---
+++ thirdparty/effekseer/EffekseerRendererDX12.h
@@ -0,0 +1,9 @@
+﻿
+#ifndef __EFFEKSEERRENDERER_DX12_H__
+#define __EFFEKSEERRENDERER_DX12_H__
+
+#include "EffekseerRendererDX12/EffekseerRendererDX12.Base.Pre.h"
+#include "EffekseerRendererDX12/EffekseerRendererDX12.Renderer.h"
+#include <EffekseerRendererLLGI/Common.h>
+
+#endif // __EFFEKSEERRENDERER_DX12_H__
---
+++ thirdparty/effekseer/LLGI/LLGI.Texture.h
@@ -0,0 +1,50 @@
+
+#pragma once
+
+#include "LLGI.Base.h"
+
+namespace LLGI
+{
+
+class Texture : public ReferenceObject
+{
+private:
+protected:
+	TextureType type_ = TextureType::Unknown;
+	TextureFormatType format_ = TextureFormatType::Unknown;
+	TextureUsageType usage_ = TextureUsageType::NoneFlag;
+
+	int32_t samplingCount_ = 1;
+	int32_t mipmapCount_ = 1;
+
+public:
+	Texture() = default;
+	~Texture() override = default;
+
+	/*[[deprecated("use CommandList::SetImageData2D.")]]*/ virtual void* Lock();
+
+	/*[[deprecated("use CommandList::SetImageData2D.")]]*/ virtual void Unlock();
+
+	/**
+		@brief	Lock an image on mipmap
+	*/
+	virtual void* Lock(int32_t mipmapLevel) { return nullptr; }
+
+	virtual bool GetData(std::vector<uint8_t>& data) { return false; }
+
+	virtual Vec2I GetSizeAs2D() const;
+	[[deprecated("use GetType.")]] virtual bool IsRenderTexture() const;
+	[[deprecated("use GetType.")]] virtual bool IsDepthTexture() const;
+
+	TextureType GetType() const { return type_; }
+
+	virtual TextureFormatType GetFormat() const;
+
+	TextureUsageType GetUsage() const;
+
+	int32_t GetSamplingCount() const { return samplingCount_; }
+
+	int32_t GetMipmapCount() const { return mipmapCount_; }
+};
+
+} // namespace LLGI
