e3318c5ea751a2be9c669458d21895a06b99019c
YutoImata <yuto.imata.wine@gmail.com>
2026-05-23 23:22:46 +0900

    feat: エフェクトの再生・停止・更新を行う純粋仮想クラス作成

---
+++ src/core/interface/IEffectFactory.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "core/constant/EffectType.h"
+#include "core/utility/Vector3.h"
+
+namespace core::iface
+{
+    /**
+     * @brief エフェクトの再生・停止・更新を行うインターフェース
+     * Game層がInfra層に依存しないように
+     */
+    class IEffectFactory
+    {
+    public:
+        virtual ~IEffectFactory() = default;
+
+        /**
+         * @brief 起動時にエフェクトプールを初期化する
+         */
+        virtual void initialize() = 0;
+
+        /**
+         * @brief エフェクトを再生する
+         * @param type エフェクトの種別
+         * @param position 再生位置
+         * @return エフェクトの識別ハンドル（stop に使用）
+         */
+        virtual int play(core::constant::EffectType type, core::Vector3 position) = 0;
+
+        /**
+         * @brief エフェクトを停止する
+         * @param handle play() が返したハンドル
+         */
+        virtual void stop(int handle) = 0;
+
+        /**
+         * @brief 再生終了したスロットの補充など毎フレームの更新処理
+         */
+        virtual void update() = 0;
+    };
+}
