508ed1eb67278c95c258fade2be895bd310deeec
YutoImata <yuto.imata.wine@gmail.com>
2026-05-31 09:10:17 +0900

    feat: Audioのイベントを購読するListenerクラスを作成

---
+++ src/game/event/AudioEventListener.cpp
@@ -0,0 +1,38 @@
+#include "AudioEventListener.h"
+#include "core/base/ServiceLocator.h"
+#include "core/interface/IAudioManager.h"
+#include "core/constant/SeType.h"
+
+namespace game::event
+{
+	AudioEventListener::AudioEventListener(core::base::EventBus& eventBus, core::ecs::EntityId playerId)
+		: m_eventBus{ eventBus }
+		, m_playerId{ playerId }
+	{
+		m_eventBus.subscribe<AttackHitEvent>(
+			[this](const AttackHitEvent& e) { onAttackHit(e); });
+
+		m_eventBus.subscribe<EnemyDeadEvent>(
+			[this](const EnemyDeadEvent& e) { onEnemyDead(e); });
+	}
+
+	void AudioEventListener::onAttackHit(const AttackHitEvent& e)
+	{
+		auto* audio{ core::base::ServiceLocator::get<core::iface::IAudioManager>() };
+		if (!audio) return;
+
+		// 攻撃SEの再生（プレイヤーがジョブに応じた攻撃音）
+		if (e.m_seType != core::constant::SeType::None)
+			audio->playSe(e.m_seType);
+
+		// プレイヤーが被弾したときのSE
+		if (e.m_targetId == m_playerId)
+			audio->playSe(core::constant::SeType::HitPlayer);
+	}
+
+	void AudioEventListener::onEnemyDead(const EnemyDeadEvent& /*e*/)
+	{
+		auto* audio{ core::base::ServiceLocator::get<core::iface::IAudioManager>() };
+		if (audio) audio->playSe(core::constant::SeType::DeadEnemy);
+	}
+}
---
+++ src/game/event/AudioEventListener.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "core/base/EventBus.h"
+#include "core/ecs/Entity.h"
+#include "game/event/InGameEvents.h"
+
+namespace game::event
+{
+	/**
+	 * @brief AttackHitEvent / EnemyDeadEvent を購読してSEを再生するリスナー
+	 */
+	class AudioEventListener
+	{
+	public:
+		AudioEventListener() = default;
+
+		/**
+		 * @brief コンストラクタ。EventBus に購読を登録する
+		 * @param eventBus EventBusの参照
+		 * @param playerId プレイヤーのEntityId（被弾SE判定に使用）
+		 */
+		AudioEventListener(core::base::EventBus& eventBus, core::ecs::EntityId playerId);
+
+	private:
+		void onAttackHit(const AttackHitEvent& e);
+		void onEnemyDead(const EnemyDeadEvent& e);
+
+		core::base::EventBus&  m_eventBus;
+		core::ecs::EntityId    m_playerId{};
+	};
+}
