d3a79ad6ba39ac6046bfa7e138006bffc8b4d9cf
YutoImata <yuto.imata.wine@gmail.com>
2026-07-24 18:47:40 +0900

    fix: サービスを登録の逆順で破棄し終了時の例外を直す

--- src/core/base/ServiceLocator.cpp
+++ src/core/base/ServiceLocator.cpp
@@ -3,4 +3,5 @@
 namespace core::base
 {
     std::unordered_map<std::type_index, std::shared_ptr<void>> ServiceLocator::m_services;
-}
+	std::vector<std::type_index> ServiceLocator::m_order;
+} // namespace core::base
