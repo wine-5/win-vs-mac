c2b77df1abc278d40773c6f3286d7e2b7fbfbbf7
YutoImata <yuto.imata.wine@gmail.com>
2026-07-25 21:53:07 +0900

    feat: HUDパネルに光が左から右へ流れる演出を追加する

--- src/game/ui/ingame/HudPanel.h
+++ src/game/ui/ingame/HudPanel.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "core/interface/IUIRenderer.h"
 #include "core/interface/IScreen.h"
+#include <chrono>
 
 namespace game::ui::ingame
 {
@@ -32,7 +33,23 @@ namespace game::ui::ingame
 		void draw(int x, int y, int width, int height);
 
 	  private:
+		/**
+		 * @brief 面の上を光の帯が左から右へ一度だけ横切る演出を描画する
+		 *
+		 * 一定間隔でだけ走らせる。常に光らせ続けると「異常が起きている」合図に見えてしまうため、
+		 * 平常時の生存確認として、間を置いて短く光らせる
+		 * @param x パネル左上のX座標
+		 * @param y パネル左上のY座標
+		 * @param width パネルの幅
+		 * @param height パネルの高さ
+		 */
+		void drawSweep(int x, int y, int width, int height);
+
 		core::iface::IUIRenderer& m_uiRenderer;
 		core::iface::IScreen& m_screen;
+
+		// 光沢の基準時刻。描画経路からしか呼ばれずdeltaTimeを受け取らないため、
+		// 経過時間は壁時計から求める
+		std::chrono::steady_clock::time_point m_startTime{ std::chrono::steady_clock::now() };
 	};
 } // namespace game::ui::ingame
