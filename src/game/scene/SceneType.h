536f7f61969ee72a83e2721c3cf637a8ad1d3f09
YutoImata <yuto.imata.wine@gmail.com>
2026-07-26 21:01:52 +0900

    feat: シーン種別をログ用の名前へ変換する関数を追加

--- src/game/scene/SceneType.h
+++ src/game/scene/SceneType.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <string_view>
 
 namespace game::scene
 {
@@ -15,4 +16,24 @@ namespace game::scene
         InGame,     // ゲーム画面
         Result      // リザルト画面
     };
+
+	/**
+	 * @brief シーン種別をログ表示用の名前へ変換する
+	 * @param sceneType シーンの種類
+	 * @return シーン名
+	 */
+	[[nodiscard]] constexpr std::string_view toString(SceneType sceneType) noexcept
+	{
+		switch (sceneType)
+		{
+		case SceneType::Bios: return "Bios";
+		case SceneType::Lockscreen: return "Lockscreen";
+		case SceneType::Title: return "Title";
+		case SceneType::Select: return "Select";
+		case SceneType::Loading: return "Loading";
+		case SceneType::InGame: return "InGame";
+		case SceneType::Result: return "Result";
+		default: return "Unknown";
+		}
+	}
 } // namespace game::scene
