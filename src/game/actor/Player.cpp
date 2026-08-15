629a91586612e44619e3cd51554b9ba027d2c56e
YutoImata <yuto.imata.wine@gmail.com>
2026-08-15 10:16:03 +0900

    feat: プレイヤーと敵に接地影を付ける

--- src/game/actor/Player.cpp
+++ src/game/actor/Player.cpp
@@ -4,6 +4,7 @@
 #include "game/component/movement/InputComponent.h"
 #include "game/component/movement/FallRecoveryComponent.h"
 #include "game/component/visual/RenderComponent.h"
+#include "game/component/visual/ShadowCasterComponent.h"
 #include "game/component/visual/AnimationComponent.h"
 #include "game/component/visual/WeaponAttachComponent.h"
 #include "game/component/combat/ColliderComponent.h"
@@ -51,6 +52,7 @@ namespace game::actor
 		componentManager.add<component::visual::AnimationComponent>(
 		    m_entity.getId(), buildAnimationComponent(playerData.getAnimations(), resourceManager));
 		componentManager.add<component::visual::RenderComponent>(m_entity.getId(), { modelHandle });
+		componentManager.add<component::visual::ShadowCasterComponent>(m_entity.getId(), {});
 		attachWeapon(componentManager, resourceManager, playerData);
 		componentManager.add<component::visual::HitEffectComponent>(m_entity.getId(), {});
 		componentManager.add<component::visual::EffectComponent>(m_entity.getId(), {});
